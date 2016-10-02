c0c1490d995450bb2824b3a9ec3ccf6c43979ad5
Erik Nelson <erik@nsk.io>
2016-10-02 12:29:58 -0400

    Sprint1 React Impl

--- client/content/actions.js
+++ client/content/actions.js
@@ -10,12 +10,16 @@ const actionTypes = {
   LOAD_NULECULE_PENDING: 'content.LOAD_NULECULE_PENDING',
   LOAD_NULECULE_FULFILLED: 'content.LOAD_NULECULE_FULFILLED',
   LOAD_NULECULE_REJECTED: 'content.LOAD_NULECULE_REJECTED',
+  ANSWER_CHANGED: 'content.ANSWER_CHANGED',
+  DEPLOY: 'content.DEPLOY',
+  DEPLOY_PENDING: 'content.DEPLOY_PENDING',
+  DEPLOY_FULFILLED: 'content.DEPLOY_FULFILLED',
+  DEPLOY_INIT: 'content.DEPLOY_INIT',
 };
 
 const actions = {
   loadNulecules: () => {
     const url = getBaseUrl() + '/nulecules';
-    console.debug('trying to hit nulecules -> ', url);
 
     return {
       type: actionTypes.LOAD_NULECULES,
@@ -24,7 +28,6 @@ const actions = {
   },
   loadNulecule: (nuleculeId) => {
     const url = getBaseUrl() + '/nulecules/' + nuleculeId;
-    console.debug('trying to load a nulecule detail -> ', url);
 
     return {
       type: actionTypes.LOAD_NULECULE,
@@ -33,6 +36,46 @@ const actions = {
         nuleculeId: nuleculeId
       }
     };
+  },
+  postAnswers: (nuleculeId, history) => {
+    const url = `${getBaseUrl()}/nulecules/${nuleculeId}`;
+    // Chaining actions with redux-promise-middleware
+    return (dispatch, getState) => { // Thunk
+      const postAnswers = getState().nulecules[nuleculeId]
+      return dispatch({ // Promise
+        type: 'POST_ANSWERS',
+        payload: axios.post(url, {nulecule: postAnswers})
+      }).then(({value, action}) => {
+        const extractIdRegex = /^cap-(.*?)$/;
+        const match = extractIdRegex.exec(action.payload.data.nulecule.general.namespace);
+        const projectId = match[1];
+
+        dispatch(actions.initDeploy(projectId));
+
+        const reviewPath = `/nulecules/${nuleculeId}/review/${projectId}`;
+        history.push(reviewPath);
+      });
+    }
+  },
+  answerChanged: (nuleculeId, nodeName, answerKey, newValue) => {
+    return {
+      type: actionTypes.ANSWER_CHANGED,
+      payload: {nuleculeId, nodeName, answerKey, newValue}
+    };
+  },
+  deploy: (nuleculeId, projectId) => {
+    const url = `${getBaseUrl()}/nulecules/${nuleculeId}/deploy`;
+    return {
+      type: actionTypes.DEPLOY,
+      payload: axios.post(url),
+      meta: { deploymentId: projectId }
+    }
+  },
+  initDeploy: (projectId) => {
+    return {
+      type: actionTypes.DEPLOY_INIT,
+      payload: { projectId }
+    }
   }
 };
 
