a0cdb9623ff49d1857bf4d86ec39ac8d0405c87a
Erik Nelson <erik@nsk.io>
2016-10-07 10:58:22 -0400

    Update vagrantfile to use new react server (#6)

--- setup/run_dev_servers_in_tmux.sh
+++ setup/run_dev_servers_in_tmux.sh
@@ -6,5 +6,5 @@ cd $CAP_SERVER
 go get
 go build
 
-tmux new-session -s "dev" -n react -d "cd /home/vagrant/cap-ui && npm start; bash -i"
+tmux new-session -s "dev" -n react -d "cd /home/vagrant/cap-ui && npm run dev; bash -i"
 tmux new-window -t "dev:1" -n "cap-server" "cd ${CAP_SERVER} && ./cap-server; bash -i"
