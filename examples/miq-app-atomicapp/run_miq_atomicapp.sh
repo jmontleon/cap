915f91feb6e1f56f6beea6440a2ff5dcbb99be50
John Matthews <jwmatthews@gmail.com>
2016-11-18 11:46:12 -0500

    Updated run_miq_atomicapp.sh to work with atomicapp 0.6.4  - verified working, able to launch from scripts and log into webui

--- examples/miq-app-atomicapp/run_miq_atomicapp.sh
+++ examples/miq-app-atomicapp/run_miq_atomicapp.sh
@@ -9,10 +9,22 @@ oadm policy add-scc-to-user privileged system:serviceaccount:miq:default
 oc login -u openshift-dev -p devel
 oc new-project miq
 mkdir -p ${DESTINATION}
-atomicapp run miq-app-atomicapp --mode fetch --destination ${DESTINATION}
-cp answers.conf ${DESTINATION}
-atomicapp run ${DESTINATION}
+atomic run miq-app-atomicapp --mode fetch --destination ${DESTINATION}
+
+export ACCESS_TOKEN=`oc whoami -t`
+sed "s/ACCESS_TOKEN/${ACCESS_TOKEN}/g" answers.conf.tmpl > answers.conf
+sudo cp answers.conf ${DESTINATION}
+if [ "$?" -ne "0" ]; then
+    exit
+fi
+
+pushd .
+cd ${DESTINATION}
+time atomic run miq-app-atomicapp . -v
+if [ "$?" -ne "0" ]; then
+    exit
+fi
+popd
 
 oc create -f ${NULECULE_LIBRARY_HOME}/extras/miq-route.yaml
-
-
+echo "Allow ~5 minutes for the miq-app container to come up and initialize"
