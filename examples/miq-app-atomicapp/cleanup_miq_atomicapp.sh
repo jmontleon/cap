b4f202ca783e5773b50a4aa496b0195fd603ff4c
John Matthews <jwmatthews@gmail.com>
2016-11-21 11:30:13 -0500

    Updated miq-atomicapp cleanup

--- examples/miq-app-atomicapp/cleanup_miq_atomicapp.sh
+++ examples/miq-app-atomicapp/cleanup_miq_atomicapp.sh
@@ -1,30 +1,28 @@
 NULECULE_LIBRARY_HOME="/home/vagrant/nulecule-library"
 DESTINATION="/tmp/miq-app-atomicapp"
 
-oc login -u openshif-dev -p devel 
-sudo atomicapp stop ${DESTINATION}
+oc login -u openshif-dev -p devel
+oc project miq
+pushd .
+cd ${DESTINATION}
+time atomic stop miq-app-atomicapp . -v
+popd
 oc delete route miq-app
+oc delete project miq
 
-
-oc login -u admin -p admin 
-oc delete pv pv02; 
-oc delete pv pv03; 
-sudo rm -rf /nfsvolumes/pv0{2,3}; 
-sudo mkdir /nfsvolumes/pv0{2,3}; 
-sudo chmod 777 /nfsvolumes/pv0{2,3}; 
-sudo chown -R nfsnobody:nfsnobody /nfsvolumes/pv0{2,3}; 
-oc create -f ${NULECULE_LIBRARY_HOME}/extras/pv02.yaml; 
-oc create -f ${NULECULE_LIBRARY_HOME}/extras/pv03.yaml; 
+oc login -u admin -p admin
+oc delete pv pv02;
+oc delete pv pv03;
+sudo rm -rf /nfsvolumes/pv0{2,3};
+sudo mkdir /nfsvolumes/pv0{2,3};
+sudo chmod 777 /nfsvolumes/pv0{2,3};
+sudo chown -R nfsnobody:nfsnobody /nfsvolumes/pv0{2,3};
+oc create -f ${NULECULE_LIBRARY_HOME}/extras/pv02.yaml;
+oc create -f ${NULECULE_LIBRARY_HOME}/extras/pv03.yaml;
 
 
 # Cleanup
-#oc delete project miq; 
-#while oc get pods | grep -q NAME ; do echo "still deleting. sleeping 2 seconds"; sleep 2; done; 
-#sleep 10; 
-#oc new-project miq; 
-#sleep 3; 
-
-#atomicapp run miq-app-atomicapp; 
-#oc create -f extras/miq-route.yaml
-
-
+#while oc get pods | grep -q NAME ; do echo "still deleting. sleeping 2 seconds"; sleep 2; done;
+#sleep 10;
+#oc new-project miq;
+#sleep 3;
