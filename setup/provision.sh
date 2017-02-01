17d3a19d894a617e9a3ada2a05606cb4e4c39450
jesus m. rodriguez <jesusr@redhat.com>
2017-02-01 16:47:02 -0500

    fix etcd directory permissions

--- setup/provision.sh
+++ setup/provision.sh
@@ -76,4 +76,4 @@ fi
 ETCD_VER=v3.1.0
 DOWNLOAD_URL=https://github.com/coreos/etcd/releases/download
 curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
-mkdir -p /tmp/test-etcd && tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
+mkdir -p /tmp/test-etcd && chown vagrant:vagrant /tmp/test-etcd && tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/test-etcd --strip-components=1
