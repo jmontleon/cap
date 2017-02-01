f208deb96e54593b5d897165b76b1f91c4988d87
jesus m. rodriguez <jesusr@redhat.com>
2017-02-01 16:47:24 -0500

    remove go get. glide took care of it now

--- setup/run_dev_servers_in_tmux.sh
+++ setup/run_dev_servers_in_tmux.sh
@@ -5,8 +5,5 @@
 
 export SERVICE_BROKER="$GOPATH/src/github.com/fusor/ansible-service-broker"
 
-# HACK: this goes away when I get client added to glide
-cd $SERVICE_BROKER && go get github.com/fsouza/go-dockerclient
-
 tmux new-session -s "dev" -n "service-broker" -d "cd $SERVICE_BROKER && make run-mock-registry"
 tmux new-window -t "dev:1" -n "mock-registry" -d "cd $SERVICE_BROKER && make run"
