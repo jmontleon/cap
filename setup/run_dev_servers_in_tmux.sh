3c28e4da47a0866f2b8248ac79f52616b4bd78c2
Erik Nelson <erik@nsk.io>
2017-02-02 14:28:02 -0500

    Label tmux panes correctly

--- setup/run_dev_servers_in_tmux.sh
+++ setup/run_dev_servers_in_tmux.sh
@@ -2,6 +2,6 @@
 
 export SERVICE_BROKER="$GOPATH/src/github.com/fusor/ansible-service-broker"
 
-tmux new-session -s "dev" -n "service-broker" -d "cd $SERVICE_BROKER && make run-mock-registry"
-tmux new-window -t "dev:1" -n "mock-registry" -d "cd $SERVICE_BROKER && make run"
+tmux new-session -s "dev" -n "ansible-service-broker" -d "cd $SERVICE_BROKER && make run"
+tmux new-window -t "dev:1" -n "mock-registry" -d "cd $SERVICE_BROKER && make run-mock-registry"
 tmux new-window -t "dev:2" -n "broker-etcd" -d "/tmp/test-etcd/etcd"
